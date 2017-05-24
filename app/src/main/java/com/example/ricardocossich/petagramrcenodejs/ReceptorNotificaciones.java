bad9a8c999926a5c72b442c1241c5eb876870601
rcossich <ricardo.cossich@gmail.com>
2017-05-24 15:26:07 -0600

    Hasta Android Wear final

--- app/src/main/java/com/example/ricardocossich/petagramrcenodejs/ReceptorNotificaciones.java
+++ app/src/main/java/com/example/ricardocossich/petagramrcenodejs/ReceptorNotificaciones.java
@@ -73,10 +73,26 @@ public class ReceptorNotificaciones extends BroadcastReceiver{
     }
 
     public void Mostrar_Perfil(Context context) {
+        String usuarioinstagram;
         Toast.makeText(context, "Se selecciona ver el perfil propio", Toast.LENGTH_LONG).show();
+        //llamar a activity_main pasando parametros
+        Intent myIntent = new Intent(context, MainActivity.class); //en luagar de Intent intent = new Intent(this, MainActivity.class);
+        myIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        usuarioinstagram = MainActivity.cuentaInstagram; //la cuenta definida como principal, es decir la que recibio like.
+        Log.d("De regreso a Main","pasando "+usuarioinstagram);
+        myIntent.putExtra("cuenta_instagram", usuarioinstagram);
+        context.startActivity(myIntent);
     }
 
     public void Otro_Perfil(Context context) {
+        String usuarioinstagram;
         Toast.makeText(context,"Se selecciona ver le perfil de usuario", Toast.LENGTH_LONG).show();
+        //llamar a activity_main pasando parametros
+        Intent myIntent = new Intent(context, MainActivity.class); //en luagar de Intent intent = new Intent(this, MainActivity.class);
+        myIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        usuarioinstagram = MainActivity.cuentaInstagramInvitado //la cuenta definida como secundarioa, es decir la que dio el like.
+        Log.d("De regreso a Main","pasando "+usuarioinstagram);
+        myIntent.putExtra("cuenta_instagram", usuarioinstagram);
+        context.startActivity(myIntent);
     }
 }
